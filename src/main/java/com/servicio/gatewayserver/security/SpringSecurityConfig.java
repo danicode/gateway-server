7a999391de719c5061966a9b144e9e404763443d
danicode <danielanzawa@gmail.com>
2024-02-13 09:41:45 -0300

    Implementando la clase de configuración Security Config

---
+++ src/main/java/com/servicio/gatewayserver/security/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.servicio.gatewayserver.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges ->
+                        exchanges
+                                .anyExchange().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
